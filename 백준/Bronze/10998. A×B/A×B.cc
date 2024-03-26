76e4e123b1b9f88be734b7c762a7fc21158c37f7
HJ <97300440+HJ-513@users.noreply.github.com>
2024-03-26 11:04:17 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10998. A×B/A×B.cc
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -1,9 +1,8 @@
-#include<stdio.h>
-main(){
-	int a=0;
-	int b=0;
-	scanf("%d %d",&a,&b);
-	if(a>0&&b<10){
-		printf("%d",a*b);
-	}
+#include <stdio.h>
+
+int main(void){
+    int a,b;
+    
+    scanf("%d %d", &a, &b);
+    printf("%d", a*b);
 }
