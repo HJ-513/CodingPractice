c8cc65f3c532d61eedf8ec6bc4f70ea9026ae4a8
HJ <97300440+HJ-513@users.noreply.github.com>
2024-04-06 15:33:16 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    int H,M;
+    scanf("%d %d", &H, &M);
+    M = (H*60)+M;
+    if (M<45){
+        M += (60*24);    
+    }
+    M -=45;
+    H = M/60;
+    M %= 60;
+    printf("%d %d", H, M);
+}
